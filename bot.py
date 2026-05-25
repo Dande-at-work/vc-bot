fb82a717a3c8b14c708f16e85c97ae3764fc32d1
Aryan Varma <aryanvarma.sav@gmail.com>
2026-05-25 20:28:26 +0530

    Add lofi music playback functionality

--- bot.py
+++ bot.py
@@ -6,15 +6,28 @@ TOKEN = os.getenv("TOKEN")
 
 VC_ID = 1505265983454056610
 
+LOFI_URL = "https://youtube.com/shorts/umoXVhKrVZQ?si=3vcZCgzboL1rvGj9"
+
 intents = discord.Intents.default()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+FFMPEG_OPTIONS = {
+    'options': '-vn'
+}
+
+YDL_OPTIONS = {
+    'format': 'bestaudio',
+    'noplaylist': True
+}
+
 
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="yapping")
-    await bot.change_presence(activity=activity)
+
+    await bot.change_presence(
+    activity=discord.CustomActivity(name="yapping")
+    )
 
     print(f"Logged in as {bot.user}")
 
@@ -23,6 +36,7 @@ async def on_ready():
 
 @tasks.loop(seconds=30)
 async def keep_alive():
+
     channel = bot.get_channel(VC_ID)
 
     if channel is None:
@@ -30,9 +44,26 @@ async def keep_alive():
         return
 
     if len(bot.voice_clients) == 0:
+
         try:
-            await channel.connect(reconnect=True)
-            print("Connected to VC")
+
+            vc = await channel.connect(reconnect=True)
+
+            import yt_dlp
+
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(LOFI_URL, download=False)
+                url2 = info['url']
+
+            source = await discord.FFmpegOpusAudio.from_probe(
+                url2,
+                **FFMPEG_OPTIONS
+            )
+
+            vc.play(source)
+
+            print("Playing lofi")
+
         except Exception as e:
             print(e)
 
